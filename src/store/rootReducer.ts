fa320db6acd71effddce196d31b27a4cf49741ad
Sebastian Velez Galeano <sebastian.velez@avenfor.com>
2023-04-24 16:30:05 -0500

    second commit

--- src/store/rootReducer.ts
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from '@reduxjs/toolkit';
-import menuReducer from "@/store/reducers/menu.slice";
-
-const rootReducer = combineReducers({
-  menu: menuReducer,
-});
-
-export default rootReducer;
